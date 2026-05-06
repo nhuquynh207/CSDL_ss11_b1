3d4feee6ce478cf2ac3e8245e0ea60cb334943d2
nhuquynh207 <tentaikhoancuato@gmail.com>
2026-05-06 22:47:08 +0700

    Add files via upload

---
+++ b1.sql
@@ -0,0 +1,17 @@
+-- bảng và dữ liệu đã đc tạo ở b4
+-- Đoạn mã nguồn hiện tại đang chạy trên Database
+DELIMITER //
+DROP PROCEDURE IF EXISTS CancelAppointment;
+CREATE PROCEDURE CancelAppointment(IN p_appointment_id INT)
+BEGIN
+--
+UPDATE Appointments
+SET status = 'Cancelled'
+WHERE appointment_id = p_appointment_id AND status = 'Pending';
+END //
+
+DELIMITER ;
+
+call CancelAppointment(1);
+-- Thủ tục CancelAppointment hiện tại chỉ nhận vào ID và thực hiện lệnh UPDATE ngay lập tức mà không có điều khoản WHERE status = 'Pending'. 
+-- Do đó, bất kể lịch khám đang ở trạng thái nào nó đều bị ghi đè thành "Đã hủy".
